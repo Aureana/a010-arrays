f6204b72798bb1bad2dfd8160929c10aa39e9cfc
Aureana <111321782+Aureana@users.noreply.github.com>
2022-09-01 20:43:53 -0300

    ammalA-Aureana-Santos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNum = [2, 13, 26, 35, 41, 60];
+const arrayString = ["batata", "alface", "queijo"];
+const arrayBooleana = [13,"b", true];
+console.log(arrayNum)
+console.log(arrayString)
+console.log(arrayBooleana)
